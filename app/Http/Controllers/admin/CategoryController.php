18545b00bbbf054e0e616a64dbc03acbd2175b9f
Vansh Srivastav <vanshsrivastavjnp25@gmail.com>
2025-11-27 16:50:43 +0530

    feat: complete Category Controller & Category API

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    // This method will return all category
+    public function index()
+    {
+        $categories = Category::orderBy('created_at', 'DESC')->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $categories
+        ]);
+    }
+
+    // This method will store a category in database
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->status = $request->status;
+        $category->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category Added Successfully.',
+            'data' => $category
+        ], 200);
+    }
+
+    // This method will return a single category
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        if ($category == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found',
+                'data' => []
+            ], 404);
+        }
+        return response()->json([
+            'status' => 200,
+            'data' => $category
+        ]);
+    }
+
+    // This method will destroy a single category
+    public function update($id, Request $request)
+    {
+        $category = Category::find($id);
+
+        if ($category == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found',
+                'data' => []
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $category->name = $request->name;
+        $category->status = $request->status;
+        $category->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category Updated Successfully',
+            'data' => $category
+        ], 200);
+    }
+
+    // This method will update a single category
+    public function destroy($id)
+    {
+
+        $category = Category::find($id);
+
+        if ($category == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found',
+                'data' => []
+            ], 404);
+        }
+
+        $category->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category Deleted Successfully'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/admin/Login', [AuthController::class, 'authenticate']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::resource('categories', CategoryController::class);
+});
+    
